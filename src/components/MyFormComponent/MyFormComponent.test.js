ddc1e2446ac8345afbccd37ce62788f401abb616
akachk <akachk@softserveinc.com>
2023-07-08 17:16:15 +0300

    UC#7 Create react unit tests

--- src/components/MyFormComponent/MyFormComponent.test.js
+++ src/components/MyFormComponent/MyFormComponent.test.js
@@ -5,179 +5,137 @@ describe('MyFormComponent', () => {
     // Mock console.log to check if it's called on form submission
     console.log = jest.fn();
 
-    beforeEach(() => {
+    const setup = () => {
         render(<MyFormComponent/>);
-    });
+        const submitButton = screen.getByText(/Submit/i);
+
+        const fillForm = (name, email, agreeTerms, gender) => {
+            fireEvent.change(screen.getByPlaceholderText(/Name/i), { target: { value: name } });
+            fireEvent.change(screen.getByPlaceholderText(/Email/i), { target: { value: email } });
+            if (agreeTerms) {
+                fireEvent.click(screen.getByTestId('agreeTerms'));
+            }
+            if(gender) {
+                fireEvent.click(screen.getByTestId(gender));
+            }
+        };
+
+        return { submitButton, fillForm };
+    };
 
     describe('Positive Test Cases', () => {
-        test('submit the form with all fields filled correctly', async () => {
-            fireEvent.change(screen.getByPlaceholderText('Name'), {
-                target: {value: 'Test Name'}
-            });
-            fireEvent.change(screen.getByPlaceholderText('Email'), {
-                target: {value: 'test@test.com'}
-            });
-            fireEvent.click(screen.getByTestId('agreeTerms'));
-            fireEvent.click(screen.getByTestId('male'));
-            fireEvent.click(screen.getByText(/Submit/i));
-
-            await waitFor(() => expect(screen.queryAllByText(/must be/i)).toHaveLength(0));
-            expect(console.log).toHaveBeenCalledWith({
+        test('submit the form with all fields filled in correctly', async () => {
+            const {fillForm, submitButton} = setup();
+            fillForm('Test Name', 'test@example.com', true, 'male');
+            fireEvent.click(submitButton);
+            await waitFor(() => expect(console.log).toHaveBeenCalledWith({
                 name: 'Test Name',
-                email: 'test@test.com',
+                email: 'test@example.com',
                 agreeTerms: true,
                 gender: 'male',
-            });
+            }));
         });
 
         test('submit the form with a very long valid name', async () => {
-            const longName = 'a'.repeat(300);
-            fireEvent.change(screen.getByPlaceholderText('Name'), {
-                target: {value: longName}
-            });
-            fireEvent.change(screen.getByPlaceholderText('Email'), {
-                target: {value: 'test@test.com'}
-            });
-            fireEvent.click(screen.getByTestId('agreeTerms'));
-            fireEvent.click(screen.getByTestId('male'));
-            fireEvent.click(screen.getByText(/Submit/i));
-
-            await waitFor(() => expect(screen.queryAllByText(/must be/i)).toHaveLength(0));
-            expect(console.log).toHaveBeenCalledWith({
-                name: longName,
-                email: 'test@test.com',
+            const {fillForm, submitButton} = setup();
+            fillForm('Test Name'.repeat(50), 'test@example.com', true, 'male');
+            fireEvent.click(submitButton);
+            await waitFor(() => expect(console.log).toHaveBeenCalledWith({
+                name: 'Test Name'.repeat(50),
+                email: 'test@example.com',
                 agreeTerms: true,
                 gender: 'male',
-            });
+            }));
         });
 
-        test('submit the form with a complex email address', async () => {
-            const complexEmail = 'test.name+alias@example.co.uk';
-            fireEvent.change(screen.getByPlaceholderText('Name'), {
-                target: {value: 'Test Name'}
-            });
-            fireEvent.change(screen.getByPlaceholderText('Email'), {
-                target: {value: complexEmail}
-            });
-            fireEvent.click(screen.getByTestId('agreeTerms'));
-            fireEvent.click(screen.getByTestId('male'));
-            fireEvent.click(screen.getByText(/Submit/i));
-
-            await waitFor(() => expect(screen.queryAllByText(/must be/i)).toHaveLength(0));
-            expect(console.log).toHaveBeenCalledWith({
+        test('submit the form with a complex valid email address', async () => {
+            const {fillForm, submitButton} = setup();
+            fillForm('Test Name', 'test.name+alias@example.co.uk', true, 'male');
+            fireEvent.click(submitButton);
+            await waitFor(() => expect(console.log).toHaveBeenCalledWith({
                 name: 'Test Name',
-                email: complexEmail,
+                email: 'test.name+alias@example.co.uk',
                 agreeTerms: true,
                 gender: 'male',
-            });
+            }));
         });
 
-        test('change the gender from male to female', async () => {
-            fireEvent.change(screen.getByPlaceholderText('Name'), {
-                target: {value: 'Test Name'}
-            });
-            fireEvent.change(screen.getByPlaceholderText('Email'), {
-                target: {value: 'test@test.com'}
-            });
-            fireEvent.click(screen.getByTestId('agreeTerms'));
-            fireEvent.click(screen.getByTestId('female'));
-            fireEvent.click(screen.getByText(/Submit/i));
-
-            await waitFor(() => expect(screen.queryAllByText(/must be/i)).toHaveLength(0));
-            expect(console.log).toHaveBeenCalledWith({
+        test('change gender and submit the form', async () => {
+            const {fillForm, submitButton} = setup();
+            fillForm('Test Name', 'test@example.com', true, 'female');
+            fireEvent.click(submitButton);
+            await waitFor(() => expect(console.log).toHaveBeenCalledWith({
                 name: 'Test Name',
-                email: 'test@test.com',
+                email: 'test@example.com',
                 agreeTerms: true,
                 gender: 'female',
-            });
+            }));
         });
 
-        test('re-submit the form after an initial successful submission', async () => {
-            fireEvent.change(screen.getByPlaceholderText('Name'), {
-                target: {value: 'Test Name'}
-            });
-            fireEvent.change(screen.getByPlaceholderText('Email'), {
-                target: {value: 'test@test.com'}
-            });
-            fireEvent.click(screen.getByTestId('agreeTerms'));
-            fireEvent.click(screen.getByTestId('male'));
-            fireEvent.click(screen.getByText(/Submit/i));
+        test('resubmit the form after initial successful submission', async () => {
+            const { fillForm, submitButton } = setup();
 
-            await waitFor(() => expect(screen.queryAllByText(/must be/i)).toHaveLength(0));
-            expect(console.log).toHaveBeenCalledWith({
+            fillForm('Test Name', 'test@example.com', true, 'male');
+            fireEvent.click(submitButton);
+
+            await waitFor(() => expect(console.log).toHaveBeenCalledWith({
                 name: 'Test Name',
-                email: 'test@test.com',
+                email: 'test@example.com',
                 agreeTerms: true,
                 gender: 'male',
-            });
+            }));
+
+            // Clear console.log mock for resubmission
             console.log.mockClear();
 
-            // Re-submit
-            fireEvent.click(screen.getByText(/Submit/i));
+            // Uncheck the 'agreeTerms' checkbox
+            fireEvent.click(screen.getByTestId('agreeTerms'));
 
-            await waitFor(() => expect(screen.queryAllByText(/must be/i)).toHaveLength(0));
-            expect(console.log).toHaveBeenCalledWith({
-                name: 'Test Name',
-                email: 'test@test.com',
+            fillForm('Another Name', 'another@example.com', true, 'female');
+            fireEvent.click(submitButton);
+
+            await waitFor(() => expect(console.log).toHaveBeenCalledWith({
+                name: 'Another Name',
+                email: 'another@example.com',
                 agreeTerms: true,
-                gender: 'male',
-            });
+                gender: 'female',
+            }));
         });
     });
 
     describe('Negative Test Cases', () => {
         test('submit the form with the Name field left blank', async () => {
-            fireEvent.click(screen.getByText(/Submit/i));
-
+            const {fillForm, submitButton} = setup();
+            fillForm('', 'test@example.com', true, 'male');
+            fireEvent.click(submitButton);
             await waitFor(() => expect(screen.getByText(/Name must be at least 3 characters./i)).toBeInTheDocument());
         });
 
         test('submit the form with an invalid email address', async () => {
-            fireEvent.change(screen.getByPlaceholderText('Email'), {
-                target: {value: 'testtest.com'}
-            });
-            fireEvent.click(screen.getByText(/Submit/i));
-
+            const {fillForm, submitButton} = setup();
+            fillForm('Test Name', 'testexample.com', true, 'male');
+            fireEvent.click(submitButton);
             await waitFor(() => expect(screen.getByText(/Email must be valid./i)).toBeInTheDocument());
         });
 
         test('submit the form without checking the Agree to Terms checkbox', async () => {
-            fireEvent.change(screen.getByPlaceholderText('Name'), {
-                target: {value: 'Test Name'}
-            });
-            fireEvent.change(screen.getByPlaceholderText('Email'), {
-                target: {value: 'test@test.com'}
-            });
-            fireEvent.click(screen.getByTestId('male'));
-            fireEvent.click(screen.getByText(/Submit/i));
-
+            const {fillForm, submitButton} = setup();
+            fillForm('Test Name', 'test@example.com', false, 'male');
+            fireEvent.click(submitButton);
             await waitFor(() => expect(screen.getByText(/You must agree to the terms./i)).toBeInTheDocument());
         });
 
         test('submit the form without selecting a gender', async () => {
-            fireEvent.change(screen.getByPlaceholderText('Name'), {
-                target: {value: 'Test Name'}
-            });
-            fireEvent.change(screen.getByPlaceholderText('Email'), {
-                target: {value: 'test@test.com'}
-            });
-            fireEvent.click(screen.getByTestId('agreeTerms'));
-            fireEvent.click(screen.getByText(/Submit/i));
-
+            const {fillForm, submitButton} = setup();
+            fillForm('Test Name', 'test@example.com', true, '');
+            fireEvent.click(submitButton);
             await waitFor(() => expect(screen.getByText(/You must select a gender./i)).toBeInTheDocument());
         });
 
         test('submit the form with a name that is less than 3 characters long', async () => {
-            fireEvent.change(screen.getByPlaceholderText('Name'), {
-                target: {value: 'Te'}
-            });
-            fireEvent.change(screen.getByPlaceholderText('Email'), {
-                target: {value: 'test@test.com'}
-            });
-            fireEvent.click(screen.getByTestId('agreeTerms'));
-            fireEvent.click(screen.getByTestId('male'));
-            fireEvent.click(screen.getByText(/Submit/i));
-
+            const {fillForm, submitButton} = setup();
+            fillForm('Te', 'test@example.com', true, 'male');
+            fireEvent.click(submitButton);
             await waitFor(() => expect(screen.getByText(/Name must be at least 3 characters./i)).toBeInTheDocument());
         });
     });
